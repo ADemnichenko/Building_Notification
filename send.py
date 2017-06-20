3fe823b0db75cca9efac74e8fcd670df422d1987
Aleksandr <aleksandr@MacBook-Pro-Aleksandr.local>
2017-06-20 18:12:57 +0300

    some update and add settings clasess

--- send.py
+++ send.py
@@ -1,18 +1,4 @@
 import smtplib
-# def connect(email, password):
-#     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-#     try:
-#         server.login(email, password)
-#         return server
-#     except smtplib.SMTPAuthenticationError:
-#         return False
-#
-# def send(email, recipients_email, msg, password):
-#     connection = connect(email, password)
-#     if connection is False:
-#         return "Authentication is failed! Check your email or password!"
-#     else:
-#         return "Message is send!" if connection.sendmail(email, recipients_email, msg) else "Message don't send!"
 
 class MailSender:
     def __init__(self, auth_email, auth_password, rcpnts_email, msg = ""):
@@ -32,7 +18,3 @@ class MailSender:
                 self.server.quit()
         except Exception as exc: #smtplib.SMTPAuthenticationError
             return "error: {0}".format(exc)  #Authentication is failed! Check your email or password!
-
-# a = MailSender("s.demnichenko@gmail.com", "prostosanya777", "s.demnichenko@gmail.com", "loshara")
-# print(a.send())
-# print(help(smtplib.SMTP_SSL))
--- setttings.py
+++ setttings.py
@@ -1,5 +1,64 @@
 import os
 import re
+
+class CheckFields:
+    def __init__(self):
+        self.r_email = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+        self.r_pass = re.compile(r"(^[a-zA-Z0-9+-]+$)")
+
+    def CheckMail(self, email):
+            if self.r_email.findall(email):
+                return True
+            else:
+                return False
+
+    def CheckPassword(self, password):
+            if self.r_pass.findall(password):
+                return True
+            else:
+                return False
+
+class UserSettings(CheckFields):
+    def __init__(self):
+        self.err_msg1 = "Fields must not be empty!"
+        self.err_msg2 = "Incorrectly login or password!"
+        self.params = {}
+
+    def saveSettings(self, auth_email, auth_password, working_dir = "", file_name = "config"):
+        c_m = self.CheckMail(auth_email)
+        c_p = self.CheckPassword(auth_password)
+
+        if c_m and c_p:
+            if auth_email != "" and auth_password != "":
+                self.params = {"login": str(auth_email), "password": str(auth_password), "directory": str(working_dir)}
+                with open(os.path.abspath(os.path.curdir) + "/" + file_name, "w") as config:
+                    for el in self.params:
+                        return True if config.write(el + "=" + self.params.get(el) + "\n") else 0
+            else:
+                return self.err_msg1
+        else:
+            return self.err_msg2
+
+    def resetSettings(self, file_name = "config"):
+        with open(os.path.abspath(os.path.curdir) + "/" + file_name, "w") as config:
+            return True if config.write("") else 0
+
+    def checkSettings(self, file_name = "config"):
+        self.params = {}
+        try:
+            with open(os.path.abspath(os.path.curdir) + "/" + file_name, "r") as config:
+                for el in config:
+                    if el.find("login=") != -1:
+                        self.params["login"] = el.replace("login=", "")
+                    if el.find("password=") != -1:
+                        self.params["password"] = el.replace("password=", "")
+                    if el.find("directory=") != -1:
+                        self.params["directory"] = el.replace("directory=", "")
+                return self.params
+        except FileNotFoundError:
+            return self.params
+
+
 #Save Settings
 def saveSettings(self, fileName):
 
