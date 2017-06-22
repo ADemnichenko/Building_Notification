ba3fdd1b879df9fefb245e0e40e6752997225e08
Aleksandr <aleksandr@MacBook-Pro-Aleksandr.local>
2017-06-23 00:15:25 +0300

    code refactoring, add build time, add signals

--- building_notification_ini.py
+++ building_notification_ini.py
@@ -1,10 +1,9 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
 import sys
-import threading
 import time
-from setttings import checkSettings, saveSettings, resetSettings
+from setttings import UserSettings
 from notification import check_build_status
 from send import MailSender
-from PyQt5 import QtWidgets, QtGui, QtCore
 from UI import  building_notification
 
 
@@ -12,29 +11,86 @@ class Build_Notification_init(building_notification.Ui_MainWindow, QtWidgets.QMa
     def __init__(self):
         super(Build_Notification_init, self).__init__()
         self.setupUi(self)
-        # Settings
-        configFileName = "config.txt"
-        checkSettings(self, configFileName)
 
-        self.save_reset_btn.button(QtWidgets.QDialogButtonBox.Save).clicked.connect(lambda: saveSettings(self, configFileName))
-        self.save_reset_btn.button(QtWidgets.QDialogButtonBox.Reset).clicked.connect(lambda: resetSettings(self, configFileName))
-        self.enter_path_btn.clicked.connect(lambda: getPath(self))
+        #Set Settings
+        self.settings = UserSettings()
+        self.settings_params = self.settings.checkSettings()
+        self.enter_enail.setText(self.settings_params.get("login", ""))
+        self.enter_enail_pass.setText(self.settings_params.get("password", ""))
+        self.enter_path_dir.setText(self.settings_params.get("directory", ""))
 
-        # Notification
-        self.sel_file_btn.clicked.connect(lambda: getFileName(self))
-        self.start_btn.button(QtWidgets.QDialogButtonBox.Ok).clicked.connect(lambda: self.statusbar.showMessage(
-            params_ini(
-                        self.enter_enail.text(),
-                        self.enter_recipients_email.text(),
-                        self.enter_enail_pass.text(),
-                        self.enter_path_file.text(),
-                        self.enter_path_dir.text()
-                       ), 5000))
+        #Threading
+        self.build_thread = Thread()
+        self.build_thread.signal.connect(self.parseLog, QtCore.Qt.QueuedConnection)
 
+        #Set params
+        self.auth_email = ""
+        self.auth_password = ""
+        self.working_dir = ""
+        self.rcpnts_email = ""
+        self.file_name = ""
+        self.file_path = ""
+        self.fails_count = 0
+        self.sucess_count = 0
 
-        # self.start_btn.button(QtWidgets.QDialogButtonBox.Cancel).clicked.connect(lambda:)
+        #Setting up events for  pressing click
+        self.save_reset_btn.button(QtWidgets.QDialogButtonBox.Save).clicked.connect(lambda: self.on_click_save())
+        self.save_reset_btn.button(QtWidgets.QDialogButtonBox.Reset).clicked.connect(lambda: self.on_click_reset())
+        self.enter_path_btn.clicked.connect(lambda: self.getPath())
+        self.sel_file_btn.clicked.connect(lambda: self.getFileName())
+        self.start_btn.button(QtWidgets.QDialogButtonBox.Ok).clicked.connect(lambda: self.on_click_ok())
+        self.start_btn.button(QtWidgets.QDialogButtonBox.Cancel).clicked.connect(lambda: self.on_click_cancel())
 
-    #Close Event
+    def parseLog(self, signal):
+        m, s = divmod(signal, 60)
+        h, m = divmod(m, 60)
+        self.timeEdit.setTime(QtCore.QTime(h, m, s))
+        if signal % 60 == 0:
+            f_c, s_c = check_build_status(self.file_path)
+            if f_c > self.fails_count : build_status = "BUILD FAILED!"
+            elif s_c > self.sucess_count : build_status = "BUILD SUCCESSFUL!"
+            else: build_status = ""
+            if build_status != "":
+                sender = MailSender(self.auth_email, self.auth_password,self.rcpnts_email, build_status)
+                sender.send()
+                self.build_thread.terminate()
+                self.start_btn.button(QtWidgets.QDialogButtonBox.Ok).setDisabled(False)
+
+    def on_click_save(self):
+        self.statusbar.showMessage(
+            self.settings.saveSettings(
+                self.enter_enail.text(),
+                self.enter_enail_pass.text(),
+                self.enter_path_dir.text()), 5000)
+
+    def on_click_reset(self):
+        if self.settings.resetSettings():
+            self.statusbar.showMessage("Reset Complete!", 5000)
+            self.enter_enail.setText("")
+            self.enter_enail_pass.setText("")
+            self.enter_path_dir.setText("")
+        else:
+            self.statusbar.showMessage("Reset Failed!", 5000)
+
+    def on_click_ok(self):
+        self.auth_email = self.enter_enail.text()
+        self.auth_password = self.enter_enail_pass.text()
+        self.working_dir = self.enter_path_dir.text()
+        self.rcpnts_email = self.enter_recipients_email.text()
+        self.file_name = self.enter_path_file.text()
+        if self.rcpnts_email != "" and self.file_name != "" and self.auth_password != "" and self.auth_email != "":
+            self.start_btn.button(QtWidgets.QDialogButtonBox.Ok).setDisabled(True)
+            self.file_path = self.working_dir.replace("\n", "") + "/" + self.file_name
+            self.fails_count, self.sucess_count = check_build_status(self.file_path)
+            self.build_thread.start()
+        else:
+            return self.statusbar.showMessage("Settings and Notification fields must not be empty!", 5000)
+
+    def on_click_cancel(self):
+        self.start_btn.button(QtWidgets.QDialogButtonBox.Ok).setDisabled(False)
+        self.build_thread.terminate()
+        self.statusbar.showMessage("Canceled...", 5000)
+
     def closeEvent(self, event):
         reply = QtWidgets.QMessageBox.question(self, 'Message',
             "Are you sure to quit?", QtWidgets.QMessageBox.Yes |
@@ -44,51 +100,36 @@ class Build_Notification_init(building_notification.Ui_MainWindow, QtWidgets.QMa
         else:
             event.ignore()
 
-def params_ini(email, r_email, password, fileName, path_dir):
-    if r_email != "" and fileName != "" and password != "" and fileName != "":
-        path = path_dir.replace("\n", "") + "/" + fileName
-        fails_count, success_count = check_build_status(path)
-        thr = threading.Thread(target=checkAndSend, args=(password, fails_count, success_count, path, email, r_email)).start()
-    else:
-        return 'Settings and Notification fields must not be empty!'
+    def getPath(self):
+        path = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory", self.enter_path_dir.text().replace("\n", ""))
+        if path != "":
+            self.statusbar.showMessage('Select path: ' + path, 5000)
+            return self.enter_path_dir.setText(path)
+        else:
+            self.statusbar.showMessage('Path don\'t select!', 5000)
+            return self.enter_path_dir.setText(self.enter_path_dir.text())
 
-#Check log and send message
-def checkAndSend(password, fails_count, success_count, path, email, recipients_email):
-    f_c, s_c = check_build_status(path)
-    if f_c > fails_count:
-        sender = MailSender(email, password, recipients_email, "BUILD FAILED!")
-        return sender.send()
-    elif s_c > success_count:
-        sender = MailSender(email, password, recipients_email, "BUILD SUCCESSFUL!")
-        return sender.send()
-    else:
-        for i in range(60):
+    def getFileName(self):
+        path, filter = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", self.enter_path_dir.text().replace("\n", ""))
+        file_name = QtCore.QFileInfo(path).fileName()
+        if file_name != "":
+            self.statusbar.showMessage('Select file: ' + file_name, 5000)
+            return self.enter_path_file.setText(file_name)
+        else:
+            self.statusbar.showMessage('File don\'t select!', 5000)
+            return self.enter_path_file.setText(self.enter_path_file.text())
+
+class Thread(QtCore.QThread):
+    signal = QtCore.pyqtSignal(int)
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    def run(self):
+        i = 0
+        while True:
             time.sleep(1)
-            print(str(i))
-        checkAndSend(password,fails_count, success_count, path, email, recipients_email)
-
-#Select path
-def getPath(self):
-    path = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory", self.enter_path_dir.text().replace("\n", ""))
-    if path != "":
-        self.statusbar.showMessage('Select path: ' + path, 5000)
-        return self.enter_path_dir.setText(path)
-    else:
-        self.statusbar.showMessage('Path don\'t select!', 5000)
-        return self.enter_path_dir.setText(self.enter_path_dir.text())
-
-#Get selected file name
-def getFileName(self):
-    path, filter = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", self.enter_path_dir.text().replace("\n", ""))
-    fileName = QtCore.QFileInfo(path).fileName()
-    if fileName != "":
-        self.statusbar.showMessage('Select file: ' + fileName, 5000)
-        return self.enter_path_file.setText(fileName)
-    else:
-        self.statusbar.showMessage('File don\'t select!', 5000)
-        return self.enter_path_file.setText(self.enter_path_file.text())
-
-
+            self.signal.emit(i)
+            i += 1
 
 if __name__=='__main__':
     qapp = QtWidgets.QApplication(sys.argv)
--- setttings.py
+++ setttings.py
@@ -7,43 +7,38 @@ class CheckFields:
         self.r_pass = re.compile(r"(^[a-zA-Z0-9+-]+$)")
 
     def CheckMail(self, email):
-            if self.r_email.findall(email):
-                return True
-            else:
-                return False
+        return True if self.r_email.findall(email) else False
 
     def CheckPassword(self, password):
-            if self.r_pass.findall(password):
-                return True
-            else:
-                return False
+        return True if self.r_pass.findall(password) else False
 
-class UserSettings(CheckFields):
+class UserSettings():
     def __init__(self):
         self.err_msg1 = "Fields must not be empty!"
         self.err_msg2 = "Incorrectly login or password!"
         self.params = {}
 
-    def saveSettings(self, auth_email, auth_password, working_dir = "", file_name = "config"):
-        c_m = self.CheckMail(auth_email)
-        c_p = self.CheckPassword(auth_password)
+    def saveSettings(self, auth_email, auth_password, working_dir = "", file_name = "config.txt"):
 
-        if c_m and c_p:
-            if auth_email != "" and auth_password != "":
+        if auth_email == "" or auth_password == "":
+            return self.err_msg1
+        else:
+            f_check = CheckFields()
+            if f_check.CheckMail(auth_email) and f_check.CheckPassword(auth_password):
                 self.params = {"login": str(auth_email), "password": str(auth_password), "directory": str(working_dir)}
                 with open(os.path.abspath(os.path.curdir) + "/" + file_name, "w") as config:
                     for el in self.params:
-                        return True if config.write(el + "=" + self.params.get(el) + "\n") else 0
+                        config.write(el + "=" + self.params.get(el) + "\n")
+                    return "Save Complete!"
             else:
-                return self.err_msg1
-        else:
-            return self.err_msg2
+                return self.err_msg2
 
-    def resetSettings(self, file_name = "config"):
-        with open(os.path.abspath(os.path.curdir) + "/" + file_name, "w") as config:
-            return True if config.write("") else 0
+    def resetSettings(self, file_name = "config.txt"):
+            with open(os.path.abspath(os.path.curdir) + "/" + file_name, "w") as config:
+                config.write("")
+            return True
 
-    def checkSettings(self, file_name = "config"):
+    def checkSettings(self, file_name = "config.txt"):
         self.params = {}
         try:
             with open(os.path.abspath(os.path.curdir) + "/" + file_name, "r") as config:
@@ -59,51 +54,51 @@ class UserSettings(CheckFields):
             return self.params
 
 
-#Save Settings
-def saveSettings(self, fileName):
-
-    mail = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
-    pas = re.compile(r"(^[a-zA-Z0-9+-]+$)")
-
-    login = self.enter_enail.text()
-    password = self.enter_enail_pass.text()
-    dir = self.enter_path_dir.text()
-
-    if login != "" and password != "" and dir != "":
-        if mail.findall(self.enter_enail.text()) and pas.findall(self.enter_enail_pass.text()):
-            params = {"login" : str(login), "password" : str(password), "directory" : str(dir)}
-            with open(os.path.abspath(os.path.curdir) + "/" + fileName, "w") as config:
-                for el in params:
-                    config.write(el+"=" + params.get(el) + "\n")
-            self.statusbar.showMessage("Save", 1000)
-        else:
-            self.statusbar.showMessage("Incorrectly login or password!", 1000)
-    else:
-        self.statusbar.showMessage("Fields must not be empty!", 1000)
-
-#Reset Settings
-def resetSettings(self, fileName):
-    with open(os.path.abspath(os.path.curdir) + "/" + fileName, "w") as config:
-        config.write("")
-    self.enter_enail.setText("")
-    self.enter_enail_pass.setText("")
-    self.enter_path_dir.setText("")
-    self.statusbar.showMessage("Reset", 1000)
-
-#Check Settings
-def checkSettings(self, fileName):
-    params = {}
-    try:
-        with open(os.path.abspath(os.path.curdir) + "/" + fileName, "r") as config:
-            for el in config:
-                if el.find("login=") != -1:
-                    params["login"] = el.replace("login=", "")
-                if el.find("password=") != -1:
-                    params["password"] = el.replace("password=", "")
-                if el.find("directory=") != -1:
-                    params["directory"] = el.replace("directory=", "")
-            self.enter_enail.setText(params.get("login", ""))
-            self.enter_enail_pass.setText(params.get("password", ""))
-            self.enter_path_dir.setText(params.get("directory", ""))
-    except FileNotFoundError:
-        return params
+# #Save Settings
+# def saveSettings(self, fileName):
+#
+#     mail = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+#     pas = re.compile(r"(^[a-zA-Z0-9+-]+$)")
+#
+#     login = self.enter_enail.text()
+#     password = self.enter_enail_pass.text()
+#     dir = self.enter_path_dir.text()
+#
+#     if login != "" and password != "" and dir != "":
+#         if mail.findall(self.enter_enail.text()) and pas.findall(self.enter_enail_pass.text()):
+#             params = {"login" : str(login), "password" : str(password), "directory" : str(dir)}
+#             with open(os.path.abspath(os.path.curdir) + "/" + fileName, "w") as config:
+#                 for el in params:
+#                     config.write(el+"=" + params.get(el) + "\n")
+#             self.statusbar.showMessage("Save", 1000)
+#         else:
+#             self.statusbar.showMessage("Incorrectly login or password!", 1000)
+#     else:
+#         self.statusbar.showMessage("Fields must not be empty!", 1000)
+#
+# #Reset Settings
+# def resetSettings(self, fileName):
+#     with open(os.path.abspath(os.path.curdir) + "/" + fileName, "w") as config:
+#         config.write("")
+#     self.enter_enail.setText("")
+#     self.enter_enail_pass.setText("")
+#     self.enter_path_dir.setText("")
+#     self.statusbar.showMessage("Reset", 1000)
+#
+# #Check Settings
+# def checkSettings(self, fileName):
+#     params = {}
+#     try:
+#         with open(os.path.abspath(os.path.curdir) + "/" + fileName, "r") as config:
+#             for el in config:
+#                 if el.find("login=") != -1:
+#                     params["login"] = el.replace("login=", "")
+#                 if el.find("password=") != -1:
+#                     params["password"] = el.replace("password=", "")
+#                 if el.find("directory=") != -1:
+#                     params["directory"] = el.replace("directory=", "")
+#             self.enter_enail.setText(params.get("login", ""))
+#             self.enter_enail_pass.setText(params.get("password", ""))
+#             self.enter_path_dir.setText(params.get("directory", ""))
+#     except FileNotFoundError:
+#         return params
