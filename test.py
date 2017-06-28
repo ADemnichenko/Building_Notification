9f68eb7163be9ef94d28723a2e2f11b656c5ed2c
Alexandr <Alexandr@gmail.com>
2017-06-28 18:38:55 +0300

    some update

--- test.py
+++ test.py
@@ -1,21 +1,44 @@
 import os
-def get_size(start_path = ""):
-    total_size = 0
-    for dirs in os.listdir(start_path):
-        for dirpath, dirnames, filenames in os.walk("{0}/{1}".format(start_path, dirs)):
-            for f in filenames:
-                fp = os.path.join(dirpath, f)
-                total_size += os.path.getsize(fp)
-        print("{0} = {1}kb".format(dirs, total_size/1000))
-        total_size = 0
-    return True
-
 import zipfile
-zip_ref = zipfile.ZipFile("", 'r')
-zip_ref.extractall("")
-zip_ref.close()
 
-for dirpath, dirnames, filenames in os.walk(""):
-    for f in filenames:
-        if f.endswith(".pak"):
-            print(f)
+class UnpakingProject:
+    def __init__(self, build_path ="C:/Users/ademnichenko/Desktop/anim_compress/IOS", unpack_path = "D:/SVN/LiS_Production/Dev/UnrealEngine-4.15.1/Engine/Binaries/Win64"):
+        self.build_path = build_path
+        self.unpak_path = unpack_path
+        self.ipa_folder_name = "extractedIPA"
+        self.pak_folder_name = "unpakedPAK"
+    def UnzipIPA(self):
+        for dir, subdirs, files in os.walk(self.build_path):
+            for filename in files:
+                if filename.endswith(".ipa"):
+                    zip_file = zipfile.ZipFile("{0}/{1}".format(self.build_path, filename), 'r')
+                    if zip_file.extractall("{0}/{1}".format(self.build_path, self.ipa_folder_name)):
+                        zip_file.close()
+                        return True
+                    else:
+                        return False
+    def UnpakPAK(self):
+        for dir, subdirs, files in os.walk("{0}/{1}".format(self.build_path,self.ipa_folder_name)):
+            for filename in files:
+                if filename.endswith(".pak"):
+                    with open("{0}/unpak.bat".format(self.build_path), 'w') as batch:
+                        batch.write("{0}/UnrealPak.exe {1}/{2} -Extract {3}/{4}".format(self.unpak_path, dir, filename, self.build_path, self.pak_folder_name))
+                    os.system("{0}/unpak.bat".format(self.build_path))
+                    return True
+    def GetPackagesSize(self):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk("{0}/{1}".format(self.build_path, self.pak_folder_name)):
+            if dirpath.find("Packages") != -1:
+                for dirname in os.listdir(dirpath):
+                    for drp, drn, fn in os.walk("{0}/{1}".format(dirpath, dirname)):
+                        for f in fn:
+                            fp = os.path.join(drp, f)
+                            total_size += os.path.getsize(fp)
+                    print("{0} = {1}Mb".format(dirname, total_size / 1000000))
+                    total_size = 0
+                break
+
+unpak = UnpakingProject()
+unpak.UnzipIPA()
+unpak.UnpakPAK()
+unpak.GetPackagesSize()
